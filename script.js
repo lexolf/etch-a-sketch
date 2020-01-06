1b1b0761ddd2ecc8481635b1495f5a94457c1f19
Alexey Volkov <grulfex@gmail.com>
2020-01-06 21:44:50 +0300

    Make colors change on mouseover

--- script.js
+++ script.js
@@ -7,10 +7,41 @@ container.style = "margin: auto auto; display: grid; grid-template-columns: repe
 var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 container.style.width = viewportHeight*0.98 + 'px';
 
+// Create square element of a grid
 var square = document.createElement('div');
 square.classList.add('square');
-square.style= "background: linear-gradient(45deg, rgba(70,19,105,1) 0%, rgba(138,20,20,1) 50%, rgba(89,64,28,1) 100%); border-radius: 10%;"
+square.style.borderRadius = "10%";
+square.style.backgroundColor = "rgba(0,0,0,0.99)";
 
+// Fill the grid with square elements (container div)
 while(container.childElementCount<16*16){
     container.appendChild(square.cloneNode(true));
 };
+
+// Generate a new color for div (gets initial color, so maybe will modify in the future to change instead of recreate)
+function generateNewColor(initialColor){
+    var stringColor = initialColor.slice(5,-1);
+    var arrayColor = stringColor.split(", ");
+    for(var i=0; i<arrayColor.length; i++) {
+        arrayColor[i] = parseFloat(arrayColor[i]);
+    };
+    for(var i=0; i<arrayColor.length-1; i++) {
+        arrayColor[i] = Math.floor(Math.random()*256)
+    };
+    var newColorString = "rgba(" + arrayColor[0] + ", " + arrayColor[1] + ", " + arrayColor[2] + ", " + arrayColor[3] + ")";
+    return newColorString;
+}
+
+// Assign new color to div
+function changeColor() {
+    var newColor = generateNewColor(this.style.backgroundColor);
+    this.style.backgroundColor = newColor;
+}
+
+var squares = document.getElementsByClassName('square');
+
+// Add event listener to every square div
+for(var i = 0; i < squares.length; i++){
+    var thisSquare =  squares[i];
+    thisSquare.addEventListener('mouseover', changeColor)
+}
