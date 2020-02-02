3d5c904e4c65c38cc08ceae5959f2bf6e793fe22
Alexey Volkov <grulfex@gmail.com>
2020-02-03 01:12:57 +0300

    Create inactive buttons

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 // Create the container element where the grid will be held
 const container = document.querySelector('#container');
-container.style = "margin: auto auto; display: grid; grid-template-columns: repeat(16, 1fr); grid-gap: 1%;";
+container.style = "margin: auto auto; display: grid; grid-template-columns: repeat(16, 1fr); grid-gap: 1%";
 
 // Container must not be wider than its height
 var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-container.style.width = viewportHeight*0.98 + 'px';
+container.style.width = viewportHeight*0.94 + 'px';
 
 // Create square element of a grid
 var square = document.createElement('div');
@@ -17,6 +17,33 @@ square.style.backgroundColor = "rgba(0,0,0,0.99)";
 while(container.childElementCount<16*16){
     container.appendChild(square.cloneNode(true));
 };
+
+// Add buttons holder
+var buttonsHolder = document.createElement('div');
+buttonsHolder.style.position = 'relative';
+buttonsHolder.style.top = viewportHeight*0.13 + 'px';
+buttonsHolder.style.textAlign = 'center';
+container.parentNode.insertBefore(buttonsHolder, container.nextSibling);
+
+// Add reset button
+var resetButton = document.createElement('button');
+resetButton.textContent = 'reset';
+resetButton.classList.add('button');
+resetButton.id = 'reset-button';
+resetButton.style.width = viewportHeight*0.15 + 'px';
+resetButton.style.height = viewportHeight*0.05 + 'px';
+resetButton.style.margin = '0 4% 4% 4%';
+buttonsHolder.appendChild(resetButton);
+
+// Add generate button
+var generateButton = document.createElement('button');
+generateButton.textContent = 'resize';
+generateButton.classList.add('button');
+generateButton.id = 'generate-button';
+generateButton.style.width = viewportHeight*0.15 + 'px';
+generateButton.style.height = viewportHeight*0.05 + 'px';
+generateButton.style.margin = '0 4% 4% 4%';
+buttonsHolder.appendChild(generateButton);
 
 // Generate a new color for div (gets initial color, so maybe will modify in the future to change instead of recreate)
 function generateNewColor(initialColor){
@@ -40,7 +67,7 @@ function changeColor() {
 
 var squares = document.getElementsByClassName('square');
 
-// Add event listener to every square div
+// Add event listener to every square div to actually call color changing
 for(var i = 0; i < squares.length; i++){
     var thisSquare =  squares[i];
     thisSquare.addEventListener('mouseover', changeColor)
